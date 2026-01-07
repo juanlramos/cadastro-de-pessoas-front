6407f5b806997e2a244fea0279d03d9c6a381818
juanlramos <juanl.bnr@gmail.com>
2026-01-07 18:26:04 -0300

    feature: error page created

--- src/pages/error/Error.tsx
+++ src/pages/error/Error.tsx
@@ -1,3 +1,18 @@
-export const Error  = () => {
-    return <h1>Error Page</h1>;
+import { Box, Typography } from "@mui/material";
+
+export const Error = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexDirection: "column",
+        height: "100vh",
+      }}
+    >
+      <Typography variant="h3">ERROR 404</Typography>
+      <Typography variant="h5">NOT FOUND</Typography>
+    </Box>
+  );
 };
